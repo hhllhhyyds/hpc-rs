bbe71bcd5a25a36c01f51155d34682cce6855ed7
hhllhhyyds <hhllhhyyds@sina.com>
2024-04-23 18:18:48 +0800

    Add cuda reduce_sum example

--- src/ptx.rs
+++ src/ptx.rs
@@ -1,2 +1,3 @@
 pub const GPU_ADD_ARRAY: &str = include_str!(concat!(env!("OUT_DIR"), "/gpu_add_array.ptx"));
 pub const HELLO_CUDA: &str = include_str!(concat!(env!("OUT_DIR"), "/hello_cuda.ptx"));
+pub const REDUCE_SUM: &str = include_str!(concat!(env!("OUT_DIR"), "/reduce_sum.ptx"));
