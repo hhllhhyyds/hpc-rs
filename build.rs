96e080d3619e9454d90a43bf5569bac1b4744f84
hhllhhyyds <hhllhhyyds@sina.com>
2024-05-29 15:13:42 +0800

    refactor: make build script more clear

--- build.rs
+++ build.rs
@@ -1,26 +1,24 @@
+const FOREIGN_LIB_DIR_NAME: &str = "lib_gen";
+const MY_CUDA_LIB_NAME: &str = "hpccuda";
+const CUDA_LIB_SEARCH_PATH: &str = "/usr/local/cuda/lib64";
+
 fn main() {
     println!("cargo::rerun-if-changed=src");
     println!("cargo::rerun-if-changed=benches");
     println!("cargo::rerun-if-changed=tests");
 
-    let lib_dir_name = "lib_gen";
-    let lib_directory =
-        std::path::Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap()).join(lib_dir_name);
-    if !std::path::Path::exists(&lib_directory) {
-        std::fs::create_dir(&lib_directory).unwrap();
+    let foreign_lib_dir = std::path::Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join(FOREIGN_LIB_DIR_NAME);
+    if !std::path::Path::exists(&foreign_lib_dir) {
+        std::fs::create_dir(&foreign_lib_dir).unwrap();
     }
 
-    let builder = bindgen_cuda::Builder::default().out_dir(&lib_directory);
+    let builder = bindgen_cuda::Builder::default().out_dir(&foreign_lib_dir);
+    builder.build_lib(foreign_lib_dir.join(format!("lib{}.a", MY_CUDA_LIB_NAME)));
 
-    let cuda_lib_name = "hpccuda";
-    builder.build_lib(lib_directory.join(format!("lib{}.a", cuda_lib_name)));
+    println!("cargo:rustc-link-search={}", foreign_lib_dir.display());
+    println!("cargo:rustc-link-search={}", CUDA_LIB_SEARCH_PATH);
 
-    println!("cargo:rustc-link-search={}", lib_directory.display());
-    println!("cargo:rustc-link-search=/usr/local/cuda/lib64");
-
-    println!("cargo:rustc-link-lib=static={}", cuda_lib_name);
-    println!("cargo:rustc-link-lib=cudart");
-
-    // println!("cargo:rustc-link-lib=stdc++"); // .cu 其实是C++, NVCC会调用g++进行编译，所以需要C++标准库
-    // println!("cargo:rustc-link-lib=cublas"); // 这是为了测试 ndarray-linalg 的 dot 函数
+    println!("cargo:rustc-link-lib=static={}", MY_CUDA_LIB_NAME);
+    println!("cargo:rustc-link-lib=static=cudart_static");
 }
